efaa91206022c45752cbce7200145c28c6872b0a
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-05-19 16:15:55 +0200

    Deploy: Python package for Linux, MacOS and Windows
    Using azure-pipelines
    
    https://pypi.org/project/wolfram-model/

---
+++ deploy/deploy_macos_one_python.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Assumes that python points to the python you want to use
+# This should be managed from other script, or from CI/CD pipelines
+
+script_dir=$(cd $(dirname $0) || exit 1; pwd)
+
+echo ""
+echo "$(python --version)"
+
+python -m pip install cmake
+python -m pip install ninja
+python -m pip install delocate
+python -m pip install -r ${script_dir}/requirements-deploy.txt
+
+pushd ${script_dir}
+python setup.py bdist_wheel --build-type Release -G Ninja -- \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9 \
+  -DCMAKE_OSX_ARCHITECTURES:STRING=x86_64 \
+  -DCMAKE_CXX_STANDARD=14 \
+  -DSET_REPLACE_BUILD_TESTING:BOOL=OFF \
+  -DSET_REPLACE_WRAP_PYTHON:BOOL=ON \
+  || exit 1
+  # ${PYBIN}/python setup.py clean
+
+delocate-listdeps $PWD/dist/*.whl # lists library dependencies
+delocate-wheel $PWD/dist/*.whl # copies library dependencies into wheel
+popd
---
+++ deploy/manylinux-build-wheels.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------
+# These variables are set in common script:
+#
+ARCH=""
+PYBINARIES=""
+
+script_dir=$(cd $(dirname $0) || exit 1; pwd)
+source "${script_dir}/manylinux-build-common.sh"
+# -----------------------------------------------------------------------
+
+pushd ${script_dir}
+# Compile wheels re-using standalone project and archive cache
+for PYBIN in "${PYBINARIES[@]}"; do
+    Python3_EXECUTABLE=${PYBIN}/python
+    Python3_INCLUDE_DIR=$( find -L ${PYBIN}/../include/ -name Python.h -exec dirname {} \; )
+    Python3_INCLUDE_DIRS=${Python3_INCLUDE_DIR}
+
+    echo ""
+    echo "Python3_EXECUTABLE:${Python3_EXECUTABLE}"
+    echo "Python3_INCLUDE_DIR:${Python3_INCLUDE_DIR}"
+
+    # Remove when scikit-build includes cmake_target PR:
+    # https://github.com/scikit-build/scikit-build/pull/477
+    ${PYBIN}/python -m pip uninstall scikit-build -y
+    ${PYBIN}/python -m pip install -r ${script_dir}/requirements-deploy.txt
+
+    ${PYBIN}/python setup.py bdist_wheel --build-type Release -G Ninja -- \
+      -DCMAKE_CXX_COMPILER_TARGET:STRING=$(uname -p)-linux-gnu \
+      -DSET_REPLACE_BUILD_TESTING:BOOL=OFF \
+      -DSET_REPLACE_WRAP_PYTHON:BOOL=ON \
+      -DPython3_EXECUTABLE:FILEPATH=${Python3_EXECUTABLE} \
+      -DPython3_INCLUDE_DIRS:PATH=${Python3_INCLUDE_DIRS} \
+    || exit 1
+    # ${PYBIN}/python setup.py clean
+done
+
+# Since there are no external shared libraries to bundle into the wheels
+# this step will fixup the wheel switching from 'linux' to 'manylinux2014' tag
+for whl in dist/*linux_$(uname -p).whl; do
+    auditwheel repair ${whl} -w /work/dist/
+    rm ${whl}
+done
+popd
